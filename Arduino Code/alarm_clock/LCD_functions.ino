bffdc81e7f6eb712ea23048add0acdf0fb3bb41d
Ilpo Härkönen <muhnuttaja@gmail.com>
2016-12-01 17:07:00 +0200

    Split the code into sub-categories and refactored LCD commands
    Main loop, LCD commands and RTC commands are now located on different files to achieve easier editing. LCD commands were refactored and split into smaller functions to avoid unnecessary code reuse.

---
+++ Arduino Code/alarm_clock/LCD_functions.ino
@@ -0,0 +1,106 @@
+///////////////////////////////
+///All LCD display functions///
+///////////////////////////////
+
+int time_row = 1;
+int hour_column = 8;
+int minute_column = hour_column + 3;
+
+//Bit art for the custom made alarm symbol. The symbol is in four pieces,
+//as it takes 4 digits of display space on the LCD screen.
+byte left_upper[8] = {B00000,B00000,B00000,B00010,B00110,B11110,B11110,B11110,};
+byte left_lower[8] = {B11110,B11110,B11110,B00110,B00010,B00000,B00000,B00000,};
+byte right_upper[8] = {B00000,B00010,B01001,B00101,B10101,B10101,B10101,B10101,};
+byte right_lower[8] = {B10101,B10101,B10101,B10101,B00101,B01001,B00010,B00000,};
+
+void initialize_alarm_symbol() {
+  //Adds the created alarm symbol pieces to the LCD character set.
+  lcd.createChar(0, left_upper);
+  lcd.createChar(1, right_upper);
+  lcd.createChar(2, left_lower);
+  lcd.createChar(3, right_lower);
+}
+
+void display_alarm_icon() {
+  //Print the custom alarm icon to the rightmost side of the screen.
+  lcd.setCursor(15,0);
+  lcd.write(byte(0));
+  lcd.setCursor(16,0);
+  lcd.write(byte(1));
+  lcd.setCursor(15,1);
+  lcd.write(byte(2));
+  lcd.setCursor(16,1);
+  lcd.write(byte(3));
+}
+
+
+void print_hour(int optional_modifier = 0) {
+  //Prints hours and a ':' -symbol after that in a format: 'HH:'
+  //LCD cursor is left where printing ends.
+  //The optional input modifier is an integer which is added to the current hour.
+  lcd.setCursor(hour_column, time_row);
+  if(current_hour <10) {
+    lcd.print('0');
+  }
+  lcd.print(current_hour + optional_modifier);
+  lcd.print(':');
+}
+
+void print_minute(int optional_modifier = 0) {
+  //Prints a ':' -symbol and minutes after that in a format: ':MM'
+  //LCD cursor is left where printing ends.
+  //The optional input modifier is an integer which is added to the current minute.
+  lcd.setCursor(minute_column - 1, time_row);
+  lcd.print(':');
+  if(current_minute <10) {
+    lcd.print('0');
+  }
+  lcd.print(current_minute + optional_modifier);
+}
+
+void display_basic() {
+  //Print out current time in HH:MM format. Also print the alarm symbol if alarm is set.
+  lcd.clear();
+  if (alarm_on == true) {
+    display_alarm_icon();
+  }
+  print_hour();
+  print_minute();
+}
+
+
+void display_menu_set_time() {
+  //Display the set time menu in the following format:    Set time:
+  //                                                        HH:MM
+
+  //Print the correct headline
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("Set time:");
+
+  //If editing hours, minutes are always printed and vice versa.
+  if (hours_chosen == true) {
+    print_minute(time_modifier_minute);
+  }
+  else {
+    print_hour(time_modifier_hour);
+  }
+
+  //IF EDITING HOURS
+  if (blinking == true && hours_chosen == true) {
+    blinking = false;
+    print_hour(time_modifier_hour);
+  }
+
+  //IF EDITING MINUTES
+  if (blinking == true && hours_chosen == false) {
+    blinking = false;
+    print_minute(time_modifier_minute);
+  }
+
+  //If the minutes or hours were not printed, we make it do so the next time this function is ran.
+  if (blinking == false) {
+    blinking = true;
+  }
+}
+
--- Arduino Code/alarm_clock/alarm_clock.ino
+++ Arduino Code/alarm_clock/alarm_clock.ino
@@ -6,7 +6,7 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
-//pins for the left and right buttons and their pressed boolean values and also the buzzer pin
+//Pins for the left and right buttons and their pressed boolean values and also the buzzer pin
 int buzzer_pin = 0;
 int LB_pin = 0;//digital pin for the left button
 int RB_pin = 0;//digital pin for the right button
@@ -17,31 +17,22 @@ unsigned int time_to_hold = 2000;//time that is needed to keep the buttons pushe
 unsigned int intervals = time_to_hold / check_interval;
 
 
-//time values
-int current_hour = 0;//rtc last reported hour
-int current_minute = 0;//rtc last reported minute
+//Time values
+//RTC last reported time values
+int current_hour = 0;
+int current_minute = 0;
+//User-inputted differences used in setting time or alarm
 int time_modifier_hour = 0;
 int time_modifier_minute = 0;
 
 
+//Booleans for blinking hours or minutes while setting them.
 boolean alarm_on = false;
 boolean hours_chosen = false;
-boolean minutes_chosen = false;
 boolean blinking = true;
 
 
-
-
-//Bit art for the custom made alarm symbol. The symbol is in four pieces,
-//as it takes 4 digits of display space on the LCD screen.
-byte left_upper[8] = {B00000,B00000,B00000,B00010,B00110,B11110,B11110,B11110,};
-byte left_lower[8] = {B11110,B11110,B11110,B00110,B00010,B00000,B00000,B00000,};
-byte right_upper[8] = {B00000,B00010,B01001,B00101,B10101,B10101,B10101,B10101,};
-byte right_lower[8] = {B10101,B10101,B10101,B10101,B00101,B01001,B00010,B00000,};
-
-
-
-
+//Different states the clock can be in. Starts out in basic.
 enum state {
   basic,//displays the time and may be readied to wake up
   choose_set_time,//shows the option to go into set_time function
@@ -50,13 +41,7 @@ enum state {
   menu_set_alarm//lets the user set alarm
 };
 
-
 enum state menu_state = basic;
-
-
-
-
-
 
 
 
@@ -67,10 +52,7 @@ void setup() {
   pinMode(RB_pin, INPUT);
   pinMode(buzzer_pin, OUTPUT);
   //initialize the custom alarm icon
-  lcd.createChar(0, left_upper);
-  lcd.createChar(1, right_upper);
-  lcd.createChar(2, left_lower);
-  lcd.createChar(3, right_lower);
+  initialize_alarm_symbol();
   //Start up the LCD display
   lcd.begin(16, 2);
   Wire.begin();
@@ -135,92 +117,10 @@ void loop() {
 
 
 
-
-  delay(500);//add delay to the loop to avoid gaining too many consecutive user inputs from a single button push
+  //Add delay to the loop to avoid gaining too many consecutive user inputs from a single button push
+  //and also determine how often the display is updated.
+  delay(500);
 }
-
-
-
-void display_basic() {
-  //Print out current time in HH:MM format. Also print the alarm symbol if alarm is set.
-  lcd.clear();
-  if (alarm_on == true) {display_alarm_icon();}
-  
-  //Place the cursor to the middle of the top row and print out the current time.
-  lcd.setCursor(8,0);
-  if(current_hour <10) {lcd.print('0');}
-  lcd.print(current_hour);
-  if(current_minute <10) {lcd.print('0');}
-  lcd.print(current_minute);
-}
-
-//FIX THE MINUTES PART IN ACCORDING TO THE HOURS PART
-void display_menu_set_time(){
-  //Display the set time menu
-  lcd.clear();
-  lcd.setCursor(2,0);
-  lcd.print('Set time:');
-
-  //IF EDITING HOURS
-  //Check if editing hours and print blinking hours
-  //Hours. Blinking print shown.
-  if (blinking == true && hours_chosen == true){
-    blinking = false;
-    lcd.setCursor(3,1);
-    if((current_hour + time_modifier_hour) <10){
-      lcd.print('0');
-    }
-    lcd.print(current_hour + time_modifier_hour);
-  }
-  
-  //Hours. Blinking print hidden.
-  if (blinking == false && hours_chosen == true){
-    blinking = true;
-    lcd.setCursor(5,1);
-    lcd.print(':');
-  }
-  
-  //Minutes
-  if (hours_chosen == true) {
-    if(current_minute + time_modifier_minute < 10){
-      lcd.print('0');
-    }
-    lcd.print(current_minute+time_modifier_minute);
-  }
-
-  
-  //IF EDITING MINUTES
-  //Check if editing minutes and print blinking minutes
-  //Blinking print shown
-  if (blinking == true && minutes_chosen == true){
-    blinking = false;
-    
-    //Hours
-    lcd.setCursor(3,1);
-    if(current_hour + time_modifier_hour <10){
-      lcd.print('0');
-    }
-    lcd.print(current_hour + time_modifier_hour);
-    lcd.print(':');
-    
-    //Minutes
-    if(current_minute + time_modifier_minute < 10){
-      lcd.print('0');
-    }
-    lcd.print(current_minute+time_modifier_minute);
-  }
-  else{
-    //Minutes without hours
-    blinking = true;
-    lcd.setCursor(3,1);
-    if(current_hour + time_modifier_hour <10){
-      lcd.print('0');
-    }
-    lcd.print(current_hour + time_modifier_hour);
-    lcd.print(':');
-  }
-}
-
 
 
 
@@ -234,24 +134,6 @@ void play_alarm_sound() {
   //delay(500);
   noTone(buzzer_pin);
 }
-
-
-
-
-void display_alarm_icon() {
-  //Print the custom alarm icon to the rightmost side of the screen.
-  lcd.setCursor(15,0);
-  lcd.write(byte(0));
-  lcd.setCursor(16,0);
-  lcd.write(byte(1));
-  lcd.setCursor(15,1);
-  lcd.write(byte(2));
-  lcd.setCursor(16,1);
-  lcd.write(byte(3));
-}
-
-
-
 
 
 
@@ -289,93 +171,5 @@ boolean check_if_pushed(int button_pin) {
   else {
     return false;
   }
-}
-
-
-
-
-//////////////////////////////////////////////////
-///All DS3231 RTC clock functions defined below///
-//////////////////////////////////////////////////
-
-
-// Convert normal decimal numbers to binary coded decimal
-byte decToBcd(byte val) {
-  return( (val/10*16) + (val%10) );
-}
-
-
-// Convert binary coded decimal to normal decimal numbers
-byte bcdToDec(byte val) {
-  return( (val/16*10) + (val%16) );
-}
-
-
-void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
-dayOfMonth, byte month, byte year) {
-  //Sets time and date data to DS3231 RTC clock
-  Wire.beginTransmission(DS3231_I2C_ADDRESS);
-  Wire.write(0); // set next input to start at the seconds register
-  Wire.write(decToBcd(second)); // set seconds
-  Wire.write(decToBcd(minute)); // set minutes
-  Wire.write(decToBcd(hour)); // set hours
-  Wire.write(decToBcd(dayOfWeek)); // set day of week (1=Sunday, 7=Saturday)
-  Wire.write(decToBcd(dayOfMonth)); // set date (1 to 31)
-  Wire.write(decToBcd(month)); // set month
-  Wire.write(decToBcd(year)); // set year (0 to 99)
-  Wire.endTransmission();
-}
-
-
-void readDS3231time(byte *second, byte *minute, byte *hour, byte *dayOfWeek,
-byte *dayOfMonth, byte *month, byte *year) {
-  Wire.beginTransmission(DS3231_I2C_ADDRESS);
-  Wire.write(0); // set DS3231 register pointer to 00h
-  Wire.endTransmission();
-  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
-  // request seven bytes of data from DS3231 starting from register 00h
-  *second = bcdToDec(Wire.read() & 0x7f);
-  *minute = bcdToDec(Wire.read());
-  *hour = bcdToDec(Wire.read() & 0x3f);
-  *dayOfWeek = bcdToDec(Wire.read());
-  *dayOfMonth = bcdToDec(Wire.read());
-  *month = bcdToDec(Wire.read());
-  *year = bcdToDec(Wire.read());
-}
-
-
-void updateTime() {
-  //Reads the time from DS3231 and updates the global variables accordingly
-  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
-  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
-  current_hour = (int) hour;
-  current_minute = (int) minute;
-}
-
-
-
-
-
-
-void set_time(int hours, int minutes) {
-  //Changes DS3231 internal time to match the given parameters.
-  byte h_byte = (byte) hours;
-  byte m_byte = (byte) minutes;
-  
-  h_byte = decToBcd(h_byte);
-  m_byte = decToBcd(m_byte);
-  setDS3231time(1, m_byte, h_byte, 1, 2, 3, 16);
-}
-
-
-void change_hour() {
- 
-}
-
-
-
-
-void change_minute() {
- 
 }
 
